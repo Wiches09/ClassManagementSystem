5ab7e7f4a052b9e7e4eba1f2f2f8905c91fec9c4
Wiches09 <109953465+Wiches09@users.noreply.github.com>
2024-02-28 01:08:57 +0700

    for testlogin

---
+++ testlogin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="POST">
+        <label for="username">
+            <h1>Username: </h1>
+        </label>
+        <input type="text" id="username">
+        <label for="password">
+            <h1>Password: </h1>
+        </label>
+        <input type="text" id="password">
+    </form>
+    <br>
+    <button>submit</button>
+</body>
+
+</html>
