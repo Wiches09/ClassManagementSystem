3ce46a38d1328670214bd495c110674d58fced0f
Rainnie-007 <rainandrain3@gmail.com>
2024-03-03 22:55:19 +0700

    add system update role

---
+++ Academic/component.js
@@ -0,0 +1,165 @@
+async function componentSidenav() {
+  const response = await fetch("../Academic/component/sidenav.php");
+  const htmlContent = await response.text();
+  document.getElementById("sidenav-container").innerHTML = htmlContent;
+
+  // Execute the JavaScript for the loaded sidebar
+  initializeSidebar();
+}
+
+function initializeSidebar() {
+  // Your existing sidebar JavaScript code here
+  const sidebarToggle = document.querySelector(".sidebar-toggle");
+  const sidebarOverlay = document.querySelector(".sidebar-overlay");
+  const sidebarMenu = document.querySelector(".sidebar-menu");
+  const main = document.querySelector(".main");
+
+  sidebarToggle.addEventListener("click", function (e) {
+    e.preventDefault();
+    main.classList.toggle("active");
+    sidebarOverlay.classList.toggle("hidden");
+    sidebarMenu.classList.toggle("-translate-x-full");
+  });
+
+  sidebarOverlay.addEventListener("click", function (e) {
+    e.preventDefault();
+    main.classList.add("active");
+    sidebarOverlay.classList.add("hidden");
+    sidebarMenu.classList.add("-translate-x-full");
+  });
+
+  document
+    .querySelectorAll(".sidebar-dropdown-toggle")
+    .forEach(function (item) {
+      item.addEventListener("click", function (e) {
+        e.preventDefault();
+        const parent = item.closest(".group");
+        if (parent.classList.contains("selected")) {
+          parent.classList.remove("selected");
+        } else {
+          document
+            .querySelectorAll(".sidebar-dropdown-toggle")
+            .forEach(function (i) {
+              i.closest(".group").classList.remove("selected");
+            });
+          parent.classList.add("selected");
+        }
+      });
+    });
+}
+
+// Call the function to load and initialize the sidebar
+componentSidenav();
+
+componentSidenav();
+
+async function componentNavbar() {
+  const response = await fetch("../Academic/component/navbar.php");
+  const htmlContent = await response.text();
+  document.getElementById("navbar-container").innerHTML = htmlContent;
+
+  initializeNavbar();
+}
+function initializeNavbar() {
+  const popperInstance = {};
+  document.querySelectorAll(".dropdown").forEach(function (item, index) {
+    const popperId = "popper-" + index;
+    const toggle = item.querySelector(".dropdown-toggle");
+    const menu = item.querySelector(".dropdown-menu");
+    menu.dataset.popperId = popperId;
+    popperInstance[popperId] = Popper.createPopper(toggle, menu, {
+      modifiers: [
+        {
+          name: "offset",
+          options: {
+            offset: [0, 8],
+          },
+        },
+        {
+          name: "preventOverflow",
+          options: {
+            padding: 24,
+          },
+        },
+      ],
+      placement: "bottom-end",
+    });
+  });
+  document.addEventListener("click", function (e) {
+    const toggle = e.target.closest(".dropdown-toggle");
+    const menu = e.target.closest(".dropdown-menu");
+    if (toggle) {
+      const menuEl = toggle
+        .closest(".dropdown")
+        .querySelector(".dropdown-menu");
+      const popperId = menuEl.dataset.popperId;
+      if (menuEl.classList.contains("hidden")) {
+        hideDropdown();
+        menuEl.classList.remove("hidden");
+        showPopper(popperId);
+      } else {
+        menuEl.classList.add("hidden");
+        hidePopper(popperId);
+      }
+    } else if (!menu) {
+      hideDropdown();
+    }
+  });
+
+  function hideDropdown() {
+    document.querySelectorAll(".dropdown-menu").forEach(function (item) {
+      item.classList.add("hidden");
+    });
+  }
+
+  function showPopper(popperId) {
+    popperInstance[popperId].setOptions(function (options) {
+      return {
+        ...options,
+        modifiers: [
+          ...options.modifiers,
+          {
+            name: "eventListeners",
+            enabled: true,
+          },
+        ],
+      };
+    });
+    popperInstance[popperId].update();
+  }
+
+  function hidePopper(popperId) {
+    popperInstance[popperId].setOptions(function (options) {
+      return {
+        ...options,
+        modifiers: [
+          ...options.modifiers,
+          {
+            name: "eventListeners",
+            enabled: false,
+          },
+        ],
+      };
+    });
+  }
+}
+
+componentNavbar();
+async function componentTotaluser() {
+  const response = await fetch("../Academic/component/totaluser.php");
+  const htmlContent = await response.text();
+  document.getElementById("totaluser-container").innerHTML = htmlContent;
+}
+componentTotaluser();
+async function componentusergraph() {
+  const response = await fetch("../Academic/component/usergraph.php");
+  const htmlContent = await response.text();
+  document.getElementById("usergraph-container").innerHTML = htmlContent;
+}
+componentusergraph();
+async function componentusertable() {
+  const response = await fetch("../Academic/component/usertable.php");
+  const htmlContent = await response.text();
+  document.getElementById("usertable-container").innerHTML = htmlContent;
+}
+componentusertable();
---
+++ Academic/component/usertable.php
@@ -0,0 +1,136 @@
+<?php
+include 'connectdatabase.php';
+?>
+
+
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6 ">
+    <div class="bg-white border border-gray-100 shadow-md shadow-black/5 p-6 rounded-md lg:col-span-2 w-full">
+        <div class="flex justify-between mb-4 items-start">
+            <div class="font-medium">USER</div>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+            <div class="rounded-md border border-dashed border-gray-200 p-4">
+                <div class="flex items-center mb-2">
+                    <div class="text-xl font-semibold">Student</div>
+                    <span class="p-1 rounded text-[12px] font-semibold bg-blue-500/10 text-blue-500 leading-none ml-2">
+                        <?php
+                        $sql = "SELECT COUNT(*) AS student_count FROM `user` WHERE `role` = 'student'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_assoc($result);
+                        echo $row['student_count'];
+                        ?>
+                    </span>
+                </div>
+
+                <table class="w-full border-collapse">
+                    <thead>
+                        <tr class="bg-gray-200">
+                            <th class="border p-2">User ID</th>
+                            <th class="border p-2">First Name</th>
+                            <th class="border p-2">Last Name</th>
+                            <th class="border p-2">Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = "SELECT * FROM `user` WHERE `role`  = 'student'";
+                        $result = mysqli_query($conn, $sql);
+
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<tr>";
+                            echo "<td class='border p-2'>" . $row['user_id'] . "</td>";
+                            echo "<td class='border p-2'>" . $row['firstname'] . "</td>";
+                            echo "<td class='border p-2'>" . $row['lastname'] . "</td>";
+                            echo "<td class='border p-2'>" . $row['role'] . "</td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="rounded-md border border-dashed border-gray-200 p-4">
+                <div class="flex items-center mb-0.5">
+                    <div class="text-xl font-semibold">Teacher</div>
+                    <span class="p-1 rounded text-[12px] font-semibold bg-emerald-500/10 text-emerald-500 leading-none ml-1">
+                        <?php
+                        $sql = "SELECT COUNT(*) AS teacher_count FROM `user` WHERE `role` = 'teacher'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_assoc($result);
+                        echo $row['teacher_count'];
+                        ?>
+                    </span>
+                </div>
+                <table class="w-full border-collapse">
+                    <thead>
+                        <tr class="bg-gray-200">
+                            <th class="border p-2">User ID</th>
+                            <th class="border p-2">First Name</th>
+                            <th class="border p-2">Last Name</th>
+                            <th class="border p-2">Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = "SELECT * FROM `user` WHERE `role`  = 'teacher'";
+                        $result = mysqli_query($conn, $sql);
+
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<tr>";
+                            echo "<td class='border p-2'>" . $row['user_id'] . "</td>";
+                            echo "<td class='border p-2'>" . $row['firstname'] . "</td>";
+                            echo "<td class='border p-2'>" . $row['lastname'] . "</td>";
+                            echo "<td class='border p-2'>" . $row['role'] . "</td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+
+
+            </div>
+            <div class="rounded-md border border-dashed border-gray-200 p-4">
+                <div class="flex items-center mb-0.5">
+                    <div class="text-xl font-semibold">User</div>
+                    <span class="p-1 rounded text-[12px] font-semibold bg-rose-500/10 text-rose-500 leading-none ml-1">
+                        <?php
+                        $sql = "SELECT COUNT(*) AS user_count FROM `user` WHERE `role` = 'user'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_assoc($result);
+                        echo $row['user_count'];
+                        ?>
+                    </span>
+                </div>
+                <table class="w-full border-collapse">
+                    <thead>
+                        <tr class="bg-gray-200">
+                            <th class="border p-2">User ID</th>
+                            <th class="border p-2">First Name</th>
+                            <th class="border p-2">Last Name</th>
+                            <th class="border p-2">Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = "SELECT * FROM `user` WHERE `role` = 'user'";
+                        $result = mysqli_query($conn, $sql);
+
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<tr>";
+                            echo "<td class='border p-2'>" . $row['user_id'] . "</td>";
+                            echo "<td class='border p-2'>" . $row['firstname'] . "</td>";
+                            echo "<td class='border p-2'>" . $row['lastname'] . "</td>";
+                            echo "<td class='border p-2'>" . $row['role'] . "</td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div>
+            <canvas id="order-chart"></canvas>
+        </div>
+    </div>
+
+</div>
